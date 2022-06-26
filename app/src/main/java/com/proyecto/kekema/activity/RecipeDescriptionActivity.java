7088a90e3e4aa2cb518c933a3163f79687ed1407
Vicky <mvictoria_29397@hotmail.es>
2022-06-26 13:01:32 +0200

    Added video to recipe

---
+++ app/src/main/java/com/proyecto/kekema/activity/RecipeDescriptionActivity.java
@@ -0,0 +1,16 @@
+package com.proyecto.kekema.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyecto.kekema.R;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
